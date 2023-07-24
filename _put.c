89937432f4c504fcbcef0dd6e207d49b90fcb9aa
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-07-24 00:43:31 +0000

    s

---
+++ _put.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+*
+* @str: the staring to print
+*
+* Return: void
+*/
+
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+* _putchar - like putchar
+*
+* @c: parametre
+*
+* Return: 1 or -1
+*/
+
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
